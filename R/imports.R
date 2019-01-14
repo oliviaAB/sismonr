1f06a93d7bf90e64a81a38ce811b4b1c73104336
OliviaAB <olivia.angelinbonnet@gmail.com>
2019-01-14 15:48:57 +1300

    Added some function imports

--- R/imports.R
+++ R/imports.R
@@ -1,6 +1,7 @@
 #' @importFrom XRJulia RJulia juliaEval juliaCommand juliaCall juliaUsing juliaGet juliaSource
-#' @importFrom stats rnorm runif
+#' @importFrom stats rnorm runif time
 #' @importFrom tictoc tic toc
 #' @importFrom magrittr %>%
 #' @importFrom dplyr mutate select bind_rows
+#' @importFrom methods getClass
 NULL
