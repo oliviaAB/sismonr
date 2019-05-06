dbeaa9f643f53141b7845ed96675cce7be2af7e6
OliviaAB <olivia.angelinbonnet@gmail.com>
2019-05-07 11:45:50 +1200

    Checked that a suitable version of Julia is installed

--- R/zzz.R
+++ R/zzz.R
@@ -17,11 +17,16 @@
   if (!XRJulia::findJulia(test = T)) {
    warning("Julia is not installed on the computer or not accessible by R. Check that Julia is correcly installed and/or in the PATH variable.\n")
   }else{
-    testFile = system.file("julia", "testModules.jl", package = "sismonr")
     julia_bin = XRJulia::findJulia()
-    if (.Platform$OS.type == "windows") cmd = paste0('"',julia_bin,'" ', testFile) else cmd = paste(julia_bin, " ", testFile)
-    packageStartupMessage("Checking if the required Julia modules are installed...")
-    hasModules = base::system(cmd, intern = FALSE, ignore.stdout = T)
-    packageStartupMessage("Done.")
+    if (.Platform$OS.type == "windows") cmd = paste(julia_bin, " ", "-E \"VERSION < v\\\"1.0.0\\\"\"") else cmd = paste(julia_bin, " ", "-E 'VERSION < v\"1.0.0\"'")
+    if(base::system(cmd, intern = T, ignore.stdout = F)){
+    warning("The current version of Julia is < to v1.0. Please install Julia v1.0 or later.")
+    }else{
+      testFile = system.file("julia", "testModules.jl", package = "sismonr")
+      if (.Platform$OS.type == "windows") cmd = paste0('"',julia_bin,'" ', testFile) else cmd = paste(julia_bin, " ", testFile)
+      packageStartupMessage("Checking if the required Julia modules are installed...")
+      hasModules = base::system(cmd, intern = FALSE, ignore.stdout = T)
+      packageStartupMessage("Done.")
+    }
   }
 }
